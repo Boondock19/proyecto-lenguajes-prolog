6c15a7fa79903cdf11eef0b01c69e474f5702f7d
Boondock19 <josematiasgonzalez20@gmail.com>
2022-07-26 19:24:45 -0400

    commit inicial con hechos hasta madres

---
+++ verdades.pl
@@ -0,0 +1,111 @@
+% Hecho : Personas
+% se leen de manera persona(X,Y)
+% donde X es una persona de genero
+% Y
+
+persona(miguelI,masc).
+persona(edudoxiaStres,fem).
+persona(alexei,masc).
+persona(maria,fem).
+persona(natalia,fem).
+persona(fiodor,masc).
+persona(sofia,fem).
+persona(ivanV,masc).
+persona(pedroI,masc).
+persona(eudociaLopu,fem).
+persona(catalinaI,fem).
+persona(praskovia,fem).
+persona(ana,fem).
+persona(ekaterina,fem).
+persona(carlos,masc).
+persona(anaLeopol,fem).
+persona(antonio,masc).
+persona(ivanVI,masc).
+persona(alexeiPedroI,masc).
+persona(carlota,fem).
+persona(pedroII,masc).
+persona(anaPedroI,fem).
+persona(carlosFederico,masc).
+persona(isabel,fem).
+persona(pedroIII,masc).
+persona(catalinaII,fem).
+persona(pablo,masc).
+persona(mariaFiodorwna,fem).
+persona(alejandroI,masc).
+persona(constantino,masc).
+persona(nicolasI,masc).
+persona(alejandra,fem).
+persona(alejandroII,masc).
+persona(mariaDeHesse,fem).
+persona(nikolai,masc).
+persona(alejandroIII,masc).
+persona(mariaFlodorovna,fem).
+persona(georgi,masc).
+persona(miguelII,masc).
+persona(nicolasII,masc).
+persona(alejandraDeHesse,fem).
+persona(alexeiNicolasII,masc).
+
+% Hecho: Padres.
+% se leen de manera padre(X,Y)
+% donde X es el padre de Y.
+
+padre(miguelI,alexei).
+padre(alexei,fiodor).
+padre(alexei,sofia).
+padre(alexei,ivanV).
+padre(alexei,pedroI).
+padre(ivanV,ana).
+padre(ivanV,ekaterina).
+padre(carlosLeopoldo,anaLeopoldovna).
+padre(antonio,ivanVI).
+padre(pedroI,alexeiPedroI).
+padre(pedroI,anaPedroI).
+padre(pedroI,isabel).
+padre(alexeiPedroI,pedroII).
+padre(carlosFederico,pedroIII).
+padre(pedroIII,pablo).
+padre(pablo,alejandroI).
+padre(pablo,constantino).
+padre(pablo,nicolasI).
+padre(nicolasI,alejandroII).
+padre(alejandroII,alejandroIII).
+padre(alejandroII,nikolai).
+padre(alejandroIII,nicolasII).
+padre(alejandroIII,georgi).
+padre(alejandroIII,miguelII).
+padre(nicolasII,alexeiNicolasII).
+
+% Hecho: Madres.
+% se leen de manera madre(X,Y)
+% donde X es la madre de Y.
+
+madre(eudoxia,alexei).
+madre(mariaMiloslavskaya,fiodor).
+madre(mariaMiloslavskaya,sofia).
+madre(mariaMiloslavskaya,ivanV).
+madre(nataliaNayshkina,pedroI).
+madre(praskoviaSaltikova,ana).
+madre(praskoviaSaltikova,ekaterina).
+madre(ekatarina,anaLeopoldovna).
+madre(anaLeopoldovna,ivanVI).
+madre(eudoxiaLopukhina,alexeiPedroI).
+madre(catalinaI,anaPedroI).
+madre(catalinaI,isabel).
+madre(carlota,pedroII).
+madre(anaPedroI,pedroIII).
+madre(catalinaII,pablo).
+madre(mariaFiodorwna,alejandroI).
+madre(mariaFiodorwna,constantino).
+madre(mariaFiodorwna,nicolasI).
+madre(alejandra,alejandroII).
+madre(mariaDeHesse,alejandroIII).
+madre(mariaDeHesse,nikolai).
+madre(mariaFiodorovna,nicolasII).
+madre(mariaFiodorovna,georgi).
+madre(mariaFiodorovna,miguelII).
+madre(alejandraDeHesse,alexeiNicolasII).
+
+
+
+
